9fae49c7a5bb088158b9c5cccc53db68f3958347
Greg Chew <gregchew@gmail.com>
2026-06-21 17:25:29 -0700

    Email: Update sender display name to LCR Admins

--- announce_games.py
+++ announce_games.py
@@ -20,7 +20,7 @@ def send_email(subject, html_content, text_content):
 
     msg = MIMEMultipart('alternative')
     msg['Subject'] = subject
-    msg['From'] = f"LCR Conciege Service <{receiver}>"
+    msg['From'] = f"LCR Admins <{receiver}>"
     msg['To'] = receiver
 
     part1 = MIMEText(text_content, 'plain')
